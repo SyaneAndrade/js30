26f56963e3636f9feaf959a768a031da3b25c693
Jessiane Gomes Andrade <syane_andrade@outlook.com>
2017-08-21 23:17:47 -0300

    Primeiro desafio terminado

---
+++ 01 - JavaScript Drum Kit/js/efects.js
@@ -0,0 +1,27 @@
+//utilizando para diminuir codigo
+function $ (param) {
+    return document.querySelector(param)
+}
+
+function playSound(e) {
+    // console.log(e.keyCode);
+    // const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    //
+    const audio = $(`audio[data-key="${e.keyCode}"]`);
+    const key = $(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function from running all together
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    key.classList.add("playing");
+}
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //skip it's not a transform
+    // console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener('keydown', playSound)
